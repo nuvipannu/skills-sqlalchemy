0b147aeb99fd746a4d74456227f27882e7dfe3f3
Nuvy Pannu <nuvypannu@Nuvys-Mac.local>
2015-05-12 15:05:57 -0700

    Finished writing SQLAlchemy queries for skills

--- query.py
+++ query.py
@@ -6,21 +6,25 @@
 Brand.query.filter_by(id = 8)
 
 # Get all models with the **name** Corvette and the **brand_name** Chevrolet.
-Brand.query.filter_by(name = "Corvette", brand_name = "Chevrolet").all()
+Model.query.filter_by(name='Corvette', brand_name='Chevrolet').all()
 
 # Get all models that are older than 1960.
-db.session.query(Model).filter
+Model.query.filter(Model.year > 1960).all()
 
 # Get all brands that were founded after 1920.
+Brand.query.filter(Brand.founded > 1920).all()
 
 # Get all models with names that begin with "Cor".
+Model.query.filter(Model.name.like('Cor%')).all()
 
 # Get all brands with that were founded in 1903 and that are not yet discontinued.
+Brand.query.filter(Brand.founded==1903, Brand.discontinued.is_(None)).all()
 
 # Get all brands with that are either discontinued or founded before 1950.
+Brand.query.filter(db.or_(Brand.founded < 1950, Brand.discontinued.isnot(None))).all()
 
 # Get any model whose brand_name is not Chevrolet.
-
+Model.query.filter(Model.brand_name != 'Chevrolet').all()
 
 # Part 2.5: Advanced and Optional
 def search_brands_by_name(mystr):
@@ -33,6 +37,8 @@ def get_models_between(start_year, end_year):
 # Part 3: Discussion Questions
 
 # 1. What is the returned value and datatype of ``Brand.query.filter_by(name='Ford')``?
+# This returns an object...
 
 # 2. In your own words, what is an association table, and what *type* of relationship 
 # does an association table manage?
+# The assoc. table is used for many-to-many relationships and connects tables that have these relationships with each other.
